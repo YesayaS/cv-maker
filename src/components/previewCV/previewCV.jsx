32a392d9700f1ac890d2ca303522baaeaba2dd9f
YesayaS <43375421+YesayaS@users.noreply.github.com>
2023-10-03 11:37:53 +0700

    feat: Auto remove empty content header

--- src/components/previewCV/previewCV.jsx
+++ src/components/previewCV/previewCV.jsx
@@ -6,8 +6,16 @@ export function PreviewCV({ basic, education, experience }) {
   return (
     <div className="m-5 flex h-a4 w-a4 flex-col bg-white p-10 font-serif">
       <PreviewBasic info={basic}></PreviewBasic>
-      <PreviewExperience info={experience}></PreviewExperience>
-      <PreviewEducation info={education}></PreviewEducation>
+      {!experience.length ? (
+        ""
+      ) : (
+        <PreviewExperience info={experience}></PreviewExperience>
+      )}
+      {!education.length ? (
+        ""
+      ) : (
+        <PreviewEducation info={education}></PreviewEducation>
+      )}
     </div>
   );
 }
